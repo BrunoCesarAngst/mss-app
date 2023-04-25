2039ac8ccddff7dcaf6d7fd97ed25cce73ca93c8
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2023-04-24 23:10:12 -0300

    teste

---
+++ mysys-app/mysys_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mysys_appApp.swift
+//  mysys-app
+//
+//  Created by Bruno Angst on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct mysys_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
